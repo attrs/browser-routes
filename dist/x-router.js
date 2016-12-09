c6943c0f69367940b1bc96e1861f468a8424eefe
joje <joje.attrs@gmail.com>
2016-12-09 13:48:19 +0900

    improve string, array polyfill

--- dist/x-router.js
+++ dist/x-router.js
@@ -83,15 +83,39 @@ return /******/ (function(modules) { // webpackBootstrap
 	    };
 	  }
 	  
-	  if( !Array.prototype.indexOf ) {
-	    Array.prototype.indexOf = function(obj, start) {
-	      for (var i = (start || 0); i < this.length; i++) {
-	        if (this[i] == obj) {
-	          return i;
-	        }
+	  if( !Array.prototype.forEach ) {
+	    Array.prototype.forEach = function(callback){
+	      for (var i = 0; i < this.length; i++){
+	        callback.apply(this, [this[i], i, this]);
 	      }
 	    };
 	  }
+	
+	  if( !Array.prototype.indexOf ) {
+	    Array.prototype.indexOf = function(obj, start) {
+	      for (var i = (start || 0); i < this.length; i++) {
+	        if (this[i] == obj) return i;
+	      }
+	      return -1;
+	    };
+	  }
+	  
+	  if( !Array.prototype.lastIndexOf ) {
+	    Array.prototype.lastIndexOf = function( item , index ){ 
+	      var index = ( index ) ? parseInt( index , 10 ) : this.length - 1; 
+	      if ( index < 0 ) index = this.length + index; 
+	      for ( index; index > -1; index-- ){  if ( this[ index ] === item ) return index;  } 
+	      return -1; 
+	    };
+	  }
+	  
+	  if( !String.prototype.trim ) {
+	    String.prototype.trim = function() {
+	      return this.replace(/^\s+|\s+$/g, ''); 
+	    };
+	  }
+	  
+	  if( !document.head ) document.head = document.getElementsByTagName("head")[0];
 	})();
 	
 	if( !document.head ) document.head = document.getElementsByTagName('head')[0];
